3f0982b06ab6a170bc40f23f0b52e82ff84d4628
Taimuraz <parabotic@gmail.com>
2019-07-30 10:52:17 +0300

    half send message done

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-import time, os
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
 # todo : registration on facebook from different users
 # todo : pin to each user a callback that executes when user recives a message.
 # todo : send private message from user.
 #
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.keys import Keys
 
-options = Options()
-options.add_argument('-private')
-browsers_number = 5
+
+
+class User:
+
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
+
+
+users = []
+users.append(User('bitakovt@gmail.com', 'Iskander1988'))
+users.append(User('taimerlance@gmail.com', 'Iskander1988'))
 
 browsers = []
-for i in range(browsers_number):
-    browsers.append(webdriver.Firefox(options=options))
-    # browsers[i].get('http://www.ubuntu.com/')
-
+for user in users:
+    browser = webdriver.Firefox(options=options)
+browsers.append(browser)
