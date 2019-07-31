e5ec958264bb2250d6fb9ff30c55d60d46d5163b
Taimuraz <parabotic@gmail.com>
2019-07-31 14:53:28 +0300

    fbchat

--- fbuser.py
+++ fbuser.py
@@ -1,29 +1,17 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.keys import Keys
+import time
 
-from time import sleep
+import threading
+from fbchat import Client
+from fbchat.models import *
 
 
-class FBUser:
+class FBUser(Client):
 
-    def __init__(self):
+    def __init__(self, login='', password=''):
         self._callback = None
-        self._login = ""
-        self._password = ""
-
-        self._options = Options()
-        self._options.add_argument('-private')
-
-        self._browser = webdriver.Firefox(options=self._options)
-
-    def set_login_password(self, login, password):
-        if type(login) == str and type(password) == str:
-            self._login = login
-            self._password = password
-        else:
-            raise TypeError("Login and password must be string.")
+        self._login = login
+        self._password = password
+        super(FBUser, self).__init__(email=login, password=password)
 
     def set_callback(self, callback):
         if callback != None:
@@ -31,70 +19,58 @@ class FBUser:
         else:
             raise ValueError("callback can't be None.")
 
-    def login(self):
-        self._browser.get("http://www.facebook.org")
-        assert "Facebook" in self._browser.title
-        elem = self._browser.find_element_by_id("email")
-        elem.send_keys(self._login)
-        elem = self._browser.find_element_by_id("pass")
-        elem.send_keys(self._password)
-        elem.send_keys(Keys.RETURN)
-        sleep(5)  # todo replace by waiting function.
-
-    def _switch_to_user_chat(self, first_name, last_name):
-        user_class_name = '_1ht6'
-
-        first_name = first_name.split()[0]  # rmove spaces
-        last_name = last_name.split()[0]  # rmove spaces
-        name = first_name + ' ' + last_name
-
-        users = self._browser.find_elements_by_xpath("//*[contains(text(), '%s')]" % name)
-        if len(users) > 0:
-            target_user = [user for user in users if user.get_attribute('class') == user_class_name][0]
-            target_user_href = target_user.find_element_by_xpath('..') \
-                .find_element_by_xpath('..') \
-                .find_element_by_xpath('..') \
-                .find_element_by_xpath('..')
-            target_user_href.click()
-        else:
-            print("INFO SEND MESSAGE: Target user not found!")
-
-    def _switch_to_textarea(self):
-        text_area = self._browser.find_elements_by_xpath("//textarea[@class='uiTextareaAutogrow _552m']")
-        print(text_area)
-
-    # todo 1.find user
-    # todo 2.find text area
-    # todo 3. type texr
-    # todo 4. click on send button.
     def send_message(self, first_name, last_name, message_text=''):
-        self._browser.get("http://www.facebook.org/messages")
-        assert "Messenger" in self._browser.title
-        self._switch_to_user_chat(first_name, last_name)
-        self._switch_to_textarea()
+        pass
+
+    def onMessage(
+            self,
+            mid=None,
+            author_id=None,
+            message=None,
+            message_object=None,
+            thread_id=None,
+            thread_type=ThreadType.USER,
+            ts=None,
+            metadata=None,
+            msg=None,
+    ):
+        self._callback(message_object.text, author_id, ts)
+
+
+class FBAssenbler:
+
+    def __init__(self):
+        self.users = {}
+        self._threads = []
+
+    def add_user(self, email, pswd, callback):
+        self.users[email] = {'email': email, 'password': pswd, 'callback': callback}
+
+    def _run(self, email):
+        fb_user = FBUser(login=email, password=self.users[email]['password'])
+        fb_user.set_callback(self.users[email]['callback'])
+        fb_user.listen()
+
+    def listen_all_users(self):
+        for email in self.users.keys():
+            self._threads = threading.Thread(target=self._run, args=(email,))
+            self._threads.start()
+
+
+
+def callback(msg_text, user_id, timestamp):
+    print(timestamp, " ", user_id, " ", msg_text)
+    print(threading.current_thread().ident)
 
 def one_user():
-    f1 = FBUser()
-    f1.set_login_password('bitakovt@gmail.com', 'Iskander1988')
-
-    f1.login()
-    f1.send_message(first_name='Petr', last_name='Vtoroy', message_text="Hellow")
-
-
-def two_usere():
-    f1 = FBUser()
-    f1.set_login_password('bitakovt@gmail.com', 'Iskander1988')
-
-    f1.login()
-    f1.send_message(first_name=' ', last_name=' ', message_text="Hellow")
-
-    f2 = FBUser()
-    f2.set_login_password('taimerlance@gmail.com', 'Iskander1988')
-
-    f2.login()
-    f2.send_message(first_name=' ', last_name=' ', message_text="Hellow")
+    # f1 = FBUser('bitakovt@gmail.com', 'Iskander1988')
+    # f1.set_callback(callback)
+    # f1.listen()
+    asmb = FBAssenbler()
+    asmb.add_user('taymerlance@gmail.com', 'Iskander1988', callback)
+    asmb.add_user('bitakovt@gmail.com', 'Iskander1988', callback)
+    asmb.listen_all_users()
 
 
 if __name__ == '__main__':
     one_user()
-    # two_usere()
--- main.py
+++ main.py
@@ -1,25 +1,44 @@
 # todo : registration on facebook from different users
 # todo : pin to each user a callback that executes when user recives a message.
 # todo : send private message from user.
-#
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.keys import Keys
+
+# users = []
+# users.append(User(, 'Iskander1988'))
+# users.append(User('taimerlance@gmail.com', ))
+
+from fbchat import Client
+from fbchat.models import *
 
 
+class MyClient(Client):
 
-class User:
+    def onMessage(
+        self,
+        mid=None,
+        author_id=None,
+        message=None,
+        message_object=None,
+        thread_id=None,
+        thread_type=ThreadType.USER,
+        ts=None,
+        metadata=None,
+        msg=None,
+    ):
+        with open('test.txt', 'w') as out_file:
+            out_file.write(10)
+        # self.markAsDelivered(thread_id, message_object.uid)
+        # self.markAsRead(thread_id)
+        #
+        #
+        # # If you're not the author, echo
+        # if author_id != self.uid:
+        #     self.send(message_object, thread_id=thread_id, thread_type=thread_type)
 
-    def __init__(self, login, password):
-        self.login = login
-        self.password = password
 
+if __name__ == '__main__':
+    client1 = MyClient('bitakovt@gmail.com', 'Iskander1988')
+    client2 = MyClient('taymerlance@gmail.com', 'Iskander1988')
 
-users = []
-users.append(User('bitakovt@gmail.com', 'Iskander1988'))
-users.append(User('taimerlance@gmail.com', 'Iskander1988'))
+    client1.send(Message(text='test message from bitakov'), thread_id=client1.uid, thread_type=ThreadType.USER)
+    client2.send(Message(text='test message from taymerlance'), thread_id=client1.uid, thread_type=ThreadType.USER)
 
-browsers = []
-for user in users:
-    browser = webdriver.Firefox(options=options)
-browsers.append(browser)
